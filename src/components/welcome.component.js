ea9e71040f539e8933ac7803f8cca539724d0cc6
Alex Wong <awong10@uh.edu>
2021-04-02 15:15:45 -0500

    testing

--- src/components/welcome.component.js
+++ src/components/welcome.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import CreateUser from './create-user.component'
+// import CreateUser from './create-user.component'
 import { Link } from 'react-router-dom'
-import UserLogin from './UserLogin'
+// import UserLogin from './UserLogin'
 
 export default class Welcome extends Component {
     render() {
@@ -18,9 +18,9 @@ export default class Welcome extends Component {
                 </div>
                     <div className="centered">
                         {'\n'}
-                        <Link to="/login" className="btn btn-primary">Login</Link>
+                        <Link id="login-link" to="/login" className="btn btn-primary">Login</Link>
                         &nbsp;&nbsp;
-                        <Link to="/create" className="btn btn-primary">Register</Link>
+                        <Link id="create-link" to="/create" className="btn btn-primary">Register</Link>
                     </div>
 
 
